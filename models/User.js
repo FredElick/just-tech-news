e77c52f37b88bceb0eae59393db408f84bc1076a
Fred <felick4000@gmail.com>
2022-07-13 14:22:33 -0400

    pass hash

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const {Model, DataTypes} = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt=require('bcrypt');
 
 class User extends Model {}
 
@@ -33,7 +34,14 @@ User.init(
 
 
     },
+
     {
+        hooks: {
+            async beforeCreate(newUserData){
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
